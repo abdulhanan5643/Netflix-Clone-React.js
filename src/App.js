83791faa193ae4579add2b4dcb43ce7e85642e43
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-11-25 01:12:12 +0500

    making Row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey!</h1>
+        <Row title="NETFLIX ORIGINALS" />
+        <Row title="TRENDING NOW" />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Row() {
+    return(
+        <div></div>
+    )
+}
+
+export default Row;
