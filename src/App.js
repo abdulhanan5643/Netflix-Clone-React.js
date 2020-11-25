6a1871a43c0820c4894f81b6b58c6b6020a64335
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-11-25 06:11:41 +0500

    All movies added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Row from './Row';
 import requests from "./requests";
+import React from "react";
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
       <h1>Hey!</h1>
         <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
         <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+        <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+        <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+        <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
