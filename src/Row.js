ca9c747510d63c2cc0ea7fdbaa4fa3bf3cd3220d
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-11-27 04:23:57 +0500

    trailer to movies added

--- src/Row.js
+++ src/Row.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from "./axios";
 import "./Row.css";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -30,7 +31,7 @@ function Row({title, fetchUrl, isLargeRow}) {
 
     const handleClick= (movie)=>{
         if (trailerUrl){
-            setTrailerUrl("");
+            setTrailerUrl('');
         }else {
             movieTrailer(movie?.name || "").then((url)=>{
                 const urlPrams=new URLSearchParams(new URL(url).search);
